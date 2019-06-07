a84651697787896ad667088dd361bcbf820136f8
Dan Caseley <dan@caseley.me.uk>
2019-06-07 16:22:49 +0100

    Simpler build, with css minify

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,12 @@
-// generated on 2019-06-02 using generator-chrome-extension 0.7.1
 const gulp = require('gulp');
-const gulpLoadPlugins = require('gulp-load-plugins');
+
+const cleanCSS = require('gulp-clean-css')
+const zip = require('gulp-zip')
 const del = require('del');
 const runSequence = require('run-sequence');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
 const es = require('event-stream');
-
-const $ = gulpLoadPlugins();
-
-gulp.task('copyEverythingElse', () => {
-  return gulp.src([
-    'app/**',
-    '!app/scripts/**'
-  ], {
-    base: 'app',
-    dot: true
-  }).pipe(gulp.dest('dist'));
-});
 
 gulp.task('buildScripts', () => {
   const files = [
@@ -29,7 +18,7 @@ gulp.task('buildScripts', () => {
   const tasks = files.map(file => (
     browserify({
       entries: `./app/scripts/${file}`,
-      debug: false
+      debug: true
     }).bundle()
       .pipe(source(file))
       .pipe(gulp.dest('dist/scripts'))
@@ -38,17 +27,34 @@ gulp.task('buildScripts', () => {
   return es.merge.apply(null, tasks);
 });
 
+gulp.task('minify-css', () => {
+  return gulp.src('app/styles/*.css')
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('dist/styles')); 
+})
+
+gulp.task('copyEverythingElse', () => {
+  return gulp.src([
+    'app/**',
+    '!app/scripts/**',
+    '!app/styles/**'
+  ], {
+    base: 'app',
+    dot: true
+  }).pipe(gulp.dest('dist'));
+});
+
 gulp.task('clean', del.bind(null, ['dist']));
 
 gulp.task('package', function () {
   var manifest = require('./dist/manifest.json');
   return gulp.src('dist/**')
-      .pipe($.zip('TempoFlex-' + manifest.version + '.zip'))
+      .pipe(zip('TempoFlex-' + manifest.version + '.zip'))
       .pipe(gulp.dest('package'));
 });
 
 gulp.task('build', (cb) => {
-  runSequence('buildScripts', 'copyEverythingElse', cb);
+  runSequence(['buildScripts', 'minify-css'], 'copyEverythingElse', cb);
 });
 
 gulp.task('default', ['clean'], cb => {
