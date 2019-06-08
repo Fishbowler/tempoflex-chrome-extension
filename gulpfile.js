62d6c8aa5ecfa644ca30ab8fb7419be2a1c8efd1
Dan Caseley <dan@caseley.me.uk>
2019-06-08 16:47:09 +0100

    Upgrade to Gulp 4 to solve vulnerabilities

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,9 @@ const gulp = require('gulp');
 const cleanCSS = require('gulp-clean-css')
 const zip = require('gulp-zip')
 const del = require('del');
-const runSequence = require('run-sequence');
 const browserify = require('browserify');
 const source = require('vinyl-source-stream');
-const es = require('event-stream');
+const merge = require('merge-stream');
 
 gulp.task('buildScripts', () => {
   const files = [
@@ -15,22 +14,21 @@ gulp.task('buildScripts', () => {
     'options.js'
   ];
 
-  const tasks = files.map(file => (
-    browserify({
-      entries: `./app/scripts/${file}`,
-      debug: true
-    }).bundle()
+  return merge(
+    files.map(file => (
+      browserify({
+        entries: `./app/scripts/${file}`,
+        debug: true
+      }).bundle()
       .pipe(source(file))
       .pipe(gulp.dest('dist/scripts'))
-  ));
-
-  return es.merge.apply(null, tasks);
+    )));
 });
 
 gulp.task('minify-css', () => {
   return gulp.src('app/styles/*.css')
     .pipe(cleanCSS())
-    .pipe(gulp.dest('dist/styles')); 
+    .pipe(gulp.dest('dist/styles'));
 })
 
 gulp.task('copyEverythingElse', () => {
@@ -44,19 +42,19 @@ gulp.task('copyEverythingElse', () => {
   }).pipe(gulp.dest('dist'));
 });
 
-gulp.task('clean', del.bind(null, ['dist']));
+gulp.task('clean', () => del('dist'));
 
 gulp.task('package', function () {
   var manifest = require('./dist/manifest.json');
   return gulp.src('dist/**')
-      .pipe(zip('TempoFlex-' + manifest.version + '.zip'))
-      .pipe(gulp.dest('package'));
+    .pipe(zip('TempoFlex-' + manifest.version + '.zip'))
+    .pipe(gulp.dest('package'));
 });
 
-gulp.task('build', (cb) => {
-  runSequence(['buildScripts', 'minify-css'], 'copyEverythingElse', cb);
-});
+gulp.task('build',
+  gulp.series(gulp.parallel('buildScripts', 'minify-css'), 'copyEverythingElse')
+)
 
-gulp.task('default', ['clean'], cb => {
-  runSequence('build', cb);
-});
+gulp.task('default',
+  gulp.series('clean', 'build')
+)
