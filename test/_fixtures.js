29795dfa66336ab235d0aad260703fe772124603
Dan Caseley <dan@caseley.me.uk>
2019-08-18 17:06:29 +0100

    More integration tests

--- test/_fixtures.js
+++ test/_fixtures.js
@@ -8,12 +8,16 @@ module.exports = {
     periodsUrl: '/rest/tempo-timesheets/4/timesheet-approval/approval-statuses/?userKey=a.smith&numberOfPeriods=1',
     worklogSearchUrl: '/rest/tempo-timesheets/4/worklogs/search',
     freezeTimeJan1st: new Date(1546354800000), //1st Jan 2019, 15:00
+    freezeTimeJan3rd: new Date(1546527600000), //3rd Jan 2019, 15:00
     worklogs: {
         oneWorklog400: [{task: 'c', timeSpentSeconds: 400}],
-        twoWorklogs700: [{task: 'a', timeSpentSeconds: 500}, {task: 'b', timeSpentSeconds: 200}]
+        twoWorklogs700: [{task: 'a', timeSpentSeconds: 500}, {task: 'b', timeSpentSeconds: 200}],
+        oneWorklog8hourday: [{task: 'all-day-slog', timeSpentSeconds: 8*60*60}]
     },
     periods: {
         twoPeriods1300Ahead: [{workedSeconds: 1001000, requiredSecondsRelativeToday: 1000000},
-            {workedSeconds: 1000300, requiredSecondsRelativeToday: 1000000}]
+                            {workedSeconds: 1000300, requiredSecondsRelativeToday: 1000000}],
+        twoPeriodsBalanced: [{workedSeconds: 1000, requiredSecondsRelativeToday: 1000},
+                            {workedSeconds: 1000, requiredSecondsRelativeToday: 1000}]
     }
 }
--- test/popupUtils.integration.test.js
+++ test/popupUtils.integration.test.js
@@ -4,14 +4,6 @@ const nock = require('nock')
 const testFixtures = require('./_fixtures')
 const timekeeper = require('timekeeper')
 
-timekeeper.freeze(testFixtures.freezeTimeJan1st) //1st Jan 2019, 15:00
-
-//Data that will "come from Tempo"
-const testPeriodData = testFixtures.periods.twoPeriods1300Ahead
-const testTodayWorklogData = testFixtures.worklogs.twoWorklogs700 //Jan 1st not a working day, so no adjustment to make
-const testFutureWorklogData = testFixtures.worklogs.oneWorklog400 //400 from Periods was future time, so will be deducted
-//1300 + 0 - 400 = 900 = 15 mins
-
 describe('getFlex', ()=>{
     beforeAll(()=>{
         global.chrome = chrome;
@@ -19,17 +11,58 @@ describe('getFlex', ()=>{
         global.XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
     })
 
-    it('will calculate flex', async ()=>{
+    afterEach(()=>{
+        timekeeper.reset()
+    })
+
+    it('will calculate flex on a non-working day', async ()=>{
+        timekeeper.freeze(testFixtures.freezeTimeJan1st) //1st Jan 2019, 15:00
+
         nock(testFixtures.settings.jiraBaseUrl)
             .get(testFixtures.periodsUrl)
-            .reply(200, testPeriodData)
+            .reply(200, testFixtures.periods.twoPeriods1300Ahead)
         nock(testFixtures.settings.jiraBaseUrl)
             .post(testFixtures.worklogSearchUrl, {worker: [testFixtures.settings.username], from:'2019-01-01', to:'2019-01-01'})
-            .reply(200, testTodayWorklogData)
+            .reply(200, testFixtures.worklogs.twoWorklogs700)
         nock(testFixtures.settings.jiraBaseUrl)
             .post(testFixtures.worklogSearchUrl, {worker: [testFixtures.settings.username], from:'2019-01-02', to:'2019-01-31'})
-            .reply(200, testFutureWorklogData)
+            .reply(200, testFixtures.worklogs.oneWorklog400)
         const flex = await popupUtils.getFlex()
         return expect(flex).toEqual('15 minutes ahead')
+        //1300 from perdiod + 0 for today (because it's not a working day) - 400 logged in the future = 900 = 15 mins
+    })
+
+    it('will calculate flex when balanced at the end of a working day', async ()=>{
+        timekeeper.freeze(testFixtures.freezeTimeJan3rd)
+        nock(testFixtures.settings.jiraBaseUrl)
+            .get(testFixtures.periodsUrl)
+            .reply(200, testFixtures.periods.twoPeriodsBalanced)
+        nock(testFixtures.settings.jiraBaseUrl)
+            .post(testFixtures.worklogSearchUrl, {worker: [testFixtures.settings.username], from:'2019-01-03', to:'2019-01-03'})
+            .reply(200, testFixtures.worklogs.oneWorklog8hourday)
+        nock(testFixtures.settings.jiraBaseUrl)
+            .post(testFixtures.worklogSearchUrl, {worker: [testFixtures.settings.username], from:'2019-01-04', to:'2019-02-02'})
+            .reply(200, [])
+        const flex = await popupUtils.getFlex()
+        return expect(flex).toEqual('Your timesheet is balanced!')
+    })
+
+    it('will fail gracefully when Jira is unreachable', async ()=>{
+        expect.assertions(1)
+        timekeeper.freeze(testFixtures.freezeTimeJan3rd)
+        nock(testFixtures.settings.jiraBaseUrl)
+            .get(testFixtures.periodsUrl)
+            .replyWithError('Nope')
+        nock(testFixtures.settings.jiraBaseUrl)
+            .post(testFixtures.worklogSearchUrl, {worker: [testFixtures.settings.username], from:'2019-01-03', to:'2019-01-03'})
+            .replyWithError('Nope')
+        nock(testFixtures.settings.jiraBaseUrl)
+            .post(testFixtures.worklogSearchUrl, {worker: [testFixtures.settings.username], from:'2019-01-04', to:'2019-02-02'})
+            .replyWithError('Nope')
+        try {
+            const flex = await popupUtils.getFlex()
+        } catch(e){
+            return expect(e).toEqual('Failed to get data from Tempo')
+        }
     })
 })
