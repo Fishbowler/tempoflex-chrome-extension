ba162aa200c269d491f81b1988ead6dbff829a34
Dan Caseley <dan@caseley.me.uk>
2020-03-29 12:27:51 +0100

    Add test data builders

--- test/_fixtures.js
+++ test/_fixtures.js
@@ -41,29 +41,91 @@ module.exports = {
     freezeTimeJan3rd: new Date(1546527600000), //3rd Jan 2019, 15:00
     freezeTimeJan4th: new Date(1546614000000), //4th Jan 2019, 15:00
     worklogs: {
-        oneWorklog400: [{task: 'c', timeSpentSeconds: 400}],
-        twoWorklogs700: [{task: 'a', timeSpentSeconds: 500}, {task: 'b', timeSpentSeconds: 200}],
-        oneWorklog8hourday: [{task: 'all-day-slog', timeSpentSeconds: 8*60*60}],
-        oneWorklog4hours: [{task: 'all-day-slog', timeSpentSeconds: 4*60*60}]
+      builder: function(){
+        let tasks = 1;
+        let time = 0;
+        return {
+          withTasks(numTasks){
+            tasks = numTasks
+            return this
+          },
+          withTime(seconds){
+            time = seconds
+            return this
+          },
+          build: function(){
+            let worklogArray = []
+            let remainingTime = time
+            for(let i = 1; i <= tasks; i++){
+              let thisTask = {}
+              thisTask.task = `task${i}`
+              if(i == tasks){
+                thisTask.timeSpentSeconds = remainingTime
+              } else {
+                thisTask.timeSpentSeconds = Math.floor(time/tasks)
+                remainingTime -= Math.floor(time/tasks)
+              }
+              worklogArray.push(thisTask)
+            }
+            return worklogArray
+          }
+        }
+      }
     },
     periods: { //TODO: Make a builder for this
-        twoPeriods1300Ahead: [{workedSeconds: 1001000, requiredSecondsRelativeToday: 1000000},
-                            {workedSeconds: 1000300, requiredSecondsRelativeToday: 1000000}],
-        twoPeriods500Behind: [{workedSeconds: 999500, requiredSecondsRelativeToday: 1000000},
-                                {workedSeconds: 1000000, requiredSecondsRelativeToday: 1000000}],
-        twoPeriodsBalanced: [{workedSeconds: 1000, requiredSecondsRelativeToday: 1000},
-                            {workedSeconds: 1000, requiredSecondsRelativeToday: 1000}],
-        onePeriod1DayAhead: [{workedSeconds: 2*8*60*60, requiredSecondsRelativeToday: 8*60*60}],
-        onePeriod1DayBehind: [{workedSeconds: 0, requiredSecondsRelativeToday: 8*60*60}],
-        onePeriod2DaysBehind: [{workedSeconds: 0, requiredSecondsRelativeToday: 2*8*60*60}],
-        onePeriod2222Behind: [{workedSeconds: (3*8*60*60)-64922, requiredSecondsRelativeToday: 3*8*60*60}],
-        onePeriodEmpty: [{workedSeconds: 0, requiredSecondsRelativeToday: 0}],
-        onePeriodUnexpected: [{workedSeconds: 'zero', requiredSecondsRelativeToday: 'zero'}]
+        onePeriodUnexpected: [{workedSeconds: 'zero', requiredSecondsRelativeToday: 'zero'}],
+        builder: function(){
+          let numberOfPeriods = 0
+          let delta = 0
+          let empty = false
+          return {
+            withPeriods: function(periods){
+              numberOfPeriods = periods
+              return this
+            },
+            withDelta: function(thisDelta){
+              delta = thisDelta
+              return this
+            },
+            empty: function(shouldBeEmpty = true){
+              empty = shouldBeEmpty
+              return this
+            },
+            build: function(){
+              let periodData = []
+              const defaultWorked = defaultRequired = 8*60*60
+
+              const buildPeriod = function(worked, required){
+                if(empty){
+                  return {workedSeconds: 0, requiredSecondsRelativeToday: 0}
+                }
+                return {workedSeconds: worked, requiredSecondsRelativeToday: required}
+              }
+              
+              if(delta < 0 && (-1 * delta) > defaultWorked){ //If the delta is behind, and by more than a day
+                periodData.push(buildPeriod(0, delta * -1)) //Make a period behind by the size of one delta
+              } else {
+                periodData.push(buildPeriod(defaultWorked + delta, defaultRequired)) //Make a period above or behind by the delta
+              }
+              
+              if(numberOfPeriods > 1){
+                for(let period = 2; period <= numberOfPeriods; period++){
+                  periodData.push(buildPeriod(defaultWorked, defaultRequired))
+                }
+              }
+
+              return periodData
+            }
+          }
+        }
     },
     userSchedules: { //TODO: Make a builder for this?
         workingDay: {"numberOfWorkingDays":1,"requiredSeconds":27000,"days":[{"date":"2019-01-03","requiredSeconds":27000,"type":"WORKING_DAY"}]},
         nonWorkingDay: {"numberOfWorkingDays":0,"requiredSeconds":0,"days":[{"date":"2019-01-01","requiredSeconds":0,"type":"NON_WORKING_DAY"}]},
-        Jan1stTo4th: {"numberOfWorkingDays":2,"requiredSeconds":2*8*60*60,"days":[{"date":"2019-01-01","requiredSeconds":0,"type":"NON_WORKING_DAY"},{"date":"2019-01-02","requiredSeconds":0,"type":"NON_WORKING_DAY"},{"date":"2019-01-03","requiredSeconds":8*60*60,"type":"WORKING_DAY"},{"date":"2019-01-04","requiredSeconds":8*60*60,"type":"WORKING_DAY"}]}
+        Jan1stTo4th: {"numberOfWorkingDays":2,"requiredSeconds":2*8*60*60,"days":[{"date":"2019-01-01","requiredSeconds":0,"type":"NON_WORKING_DAY"},{"date":"2019-01-02","requiredSeconds":0,"type":"NON_WORKING_DAY"},{"date":"2019-01-03","requiredSeconds":8*60*60,"type":"WORKING_DAY"},{"date":"2019-01-04","requiredSeconds":8*60*60,"type":"WORKING_DAY"}]},
+        build: function(secondsPerDay, startDate, endDate){
+          //args aren't right...
+        }
     },
     unexpected: {
       emoji: '🥔',
