d469a72ed52cd3ba029fe8322205663f5561981a
Dan Caseley <dan@caseley.me.uk>
2020-10-30 14:07:40 +0000

    Switch to random numbers
    Mozilla don't like version segments >5 chars

--- util/test_versioning.js
+++ util/test_versioning.js
@@ -3,21 +3,12 @@ const path = require('path')
 const fileName = path.resolve(__dirname, '../dist/manifest.json')
 const manifest = require(fileName)
     
-manifest.version = manifest.version + '.' + getDateString()
+manifest.version = manifest.version + '.' + getRandomString()
 
 fs.writeFileSync(fileName, JSON.stringify(manifest, null, 2))
 
-function getDateString(){
-    const now = new Date()
-    let dateString = ''
-    dateString += now.getFullYear()
-    dateString += padForTwoDigits(now.getMonth() + 1)
-    dateString += padForTwoDigits(now.getDate())
-    dateString += padForTwoDigits(now.getHours())
-    dateString += padForTwoDigits(now.getMinutes())
-    return dateString
-}
-
-function padForTwoDigits(value){
-    return ('0' + value).slice(-2)
+function getRandomString(){
+    const randomFiveDigitNumber = Math.floor(Math.random() * 99999) + 1
+    const randomFiveDigitString = ('0000' + randomFiveDigitNumber).slice(-5)
+    return randomFiveDigitString
 }
